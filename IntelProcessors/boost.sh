64aaeccd7e39ec4576c24110bb0485f3d897e936
Matheus Marques <smmarques@blaise>
2022-04-12 10:45:31 -0300

    boost.sh corrected

--- IntelProcessors/boost.sh
+++ IntelProcessors/boost.sh
@@ -4,15 +4,14 @@ TURBO_DISABLED="0"
 TURBO_ENABLED="1"
 ACTUAL_STATE=$(</sys/devices/system/cpu/cpufreq/boost)
         perf stat -e instructions,cycles -o out.txt sleep 1.0
-while ps -p $1 > /dev/null:
+while ps -p $1 > process.txt:
 do
         sed -i 's/,/./g' out.txt
         ipc=$(egrep "insn" out.txt | awk '{print $4}')
         #echo "ipc = $ipc"
-        
         if [ $(echo "$ipc>$IPC_TARGET"| bc) -eq 0 ] && [ $ACTUAL_STATE -eq $TURBO_ENABLED ] ;
         then
-                ACTUAL_STATE=$TURBO_DISABLED    
+                ACTUAL_STATE=$TURBO_DISABLED
                 echo $TURBO_DISABLED > /sys/devices/system/cpu/cpufreq/boost
 
         elif [ $(echo "$ipc<=$IPC_TARGET"| bc) -eq 0 ] && [ $ACTUAL_STATE -eq $TURBO_DISABLED ];
